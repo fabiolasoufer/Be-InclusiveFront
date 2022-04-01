82419a0a4b879d0f914c1baea0e59ea687d55386
Fabiola <fabiolasou.za@live.com>
2022-04-01 11:45:35 -0300

    @fabiolasoufer [Task: Criando camadas Models]

---
+++ src/app/models/PostagemModel.ts
@@ -0,0 +1,11 @@
+import { Tema } from "./Tema"
+import { Usuario } from "./Usuario"
+
+export class PostagemModel {
+    public id: number
+    public titulo: string
+    public descricao: string
+    public data: Date
+    public usuario: Usuario
+    public tema: Tema
+}
